bbb42a2a17502f2b04e2b274dd7252120701f746
Paula Frías <paula.frias.navarro@gmail.com>
2021-02-17 20:14:21 -0300

    header, banner, carousell, footer done

--- src/Components/Banner/banner.js
+++ src/Components/Banner/banner.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React from "react";
+import './style.css';
+
+import banner from "../../statics/Banner.jpg";
+
 const Banner = () => {
-    return ( <div>
-        banner
-    </div> );
-}
- 
+  return (
+    <div className="container-img-banner">
+      <img className="img-banner" alt="banner" src={banner} />
+    </div>
+  );
+};
+
 export default Banner;
